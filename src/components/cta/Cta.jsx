2453635af6877e16879eb20780359058f71777dc
pnhoanganh <pnhoanganhh@gmail.com>
2024-11-23 23:33:28 +0700

    done UI Features, Possiblity, CTA

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -1,7 +1,19 @@
 import "./cta.css";
 
 const Cta = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="gpt3__CTA section__magrin">
+        <div className="gpt3__CTA-content">
+          <p>Request Early Access to Get Started</p>
+          <h2>Register today & start exploring the endless possiblities.</h2>
+        </div>
+        <div className="gpt3__CTA-button">
+          <button>Get Started</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Cta;
